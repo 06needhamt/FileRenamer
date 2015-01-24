a048379a40abafe5e25545d3c3248ec512ca322e
06needhamt <06needhamt@gmail.com>
2015-01-24 10:50:02 +0000

    project

---
+++ FileRenamer/FileRenamer/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FileRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pgbProgress = new System.Windows.Forms.ProgressBar();
+            this.btnRenameFiles = new System.Windows.Forms.Button();
+            this.dirSelect = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 85);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(140, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(181, 85);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pgbProgress
+            // 
+            this.pgbProgress.Location = new System.Drawing.Point(26, 183);
+            this.pgbProgress.Name = "pgbProgress";
+            this.pgbProgress.Size = new System.Drawing.Size(230, 23);
+            this.pgbProgress.TabIndex = 2;
+            // 
+            // btnRenameFiles
+            // 
+            this.btnRenameFiles.Location = new System.Drawing.Point(93, 227);
+            this.btnRenameFiles.Name = "btnRenameFiles";
+            this.btnRenameFiles.Size = new System.Drawing.Size(99, 23);
+            this.btnRenameFiles.TabIndex = 3;
+            this.btnRenameFiles.Text = "Rename Files";
+            this.btnRenameFiles.UseVisualStyleBackColor = true;
+            this.btnRenameFiles.Click += new System.EventHandler(this.btnRenameFiles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnRenameFiles);
+            this.Controls.Add(this.pgbProgress);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ProgressBar pgbProgress;
+        private System.Windows.Forms.Button btnRenameFiles;
+        private System.Windows.Forms.FolderBrowserDialog dirSelect;
+    }
+}
+
---
+++ FileRenamer/FileRenamer/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace FileRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            dirSelect.ShowDialog(this);
+            txtPath.Text = dirSelect.SelectedPath;
+        }
+
+        private void btnRenameFiles_Click(object sender, EventArgs e)
+        {
+            int filecount = Directory.EnumerateFiles(txtPath.Text).Count();
+            pgbProgress.Maximum = filecount;
+            string directory = txtPath.Text;
+            foreach(var file in new DirectoryInfo(directory).GetFiles("*.*"))
+            {
+                //file.Delete();
+                if(HasUpperCase(file.Name))
+                {
+                    File.Move(directory + "/" +  file.Name, directory + "/" + file.Name.ToLower());
+                    pgbProgress.Increment(1);
+                }
+                else
+                {
+                    pgbProgress.Increment(1);
+                    continue;
+                }
+            }
+            MessageBox.Show(filecount + " Files were renamed");
+        }
+
+        bool HasUpperCase(string str)
+        {
+            return !string.IsNullOrEmpty(str) && str.Any(c => char.IsUpper(c));
+        }
+    }
+}
